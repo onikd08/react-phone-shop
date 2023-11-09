260b544d0a2083a116a04bf5dad62ea56b604031
Anik Das <onikd08@gmail.com>
2023-11-09 09:59:15 +0200

    Fix styles in PhoneCard

--- src/components/PhoneCard/PhoneCard.jsx
+++ src/components/PhoneCard/PhoneCard.jsx
@@ -7,23 +7,23 @@ const PhoneCard = ({ phone }) => {
   return (
     <div className="card bg-base-100 shadow-xl rounded-xl">
       <figure className="px-10 pt-10">
-        <img src={image} alt={phone_name} className="rounded-xl" />
+        <img src={image} alt={phone_name} className="rounded-xl h-[222px]" />
       </figure>
-      <div className="m-4">
+      <div className="m-4 flex-grow">
         <p className="text-center text-lg mb-1">{brand_name}</p>
         <h2 className="text-xl font-semibold text-center">{phone_name}</h2>
-        <div className="flex justify-between my-2">
+        <div className="flex-grow  flex justify-between my-2">
           <p>Price: {price}$</p>
           <p>Rating: {rating}</p>
         </div>
-        <div className="card-actions justify-center">
-          <Link
-            to={`/phone/${id}`}
-            className="bg-[#9333ea] my-2 text-white rounded-lg px-4 py-2"
-          >
-            See Details
-          </Link>
-        </div>
+      </div>
+      <div className="flex justify-center">
+        <Link
+          to={`/phone/${id}`}
+          className="bg-[#9333ea] text-white rounded-lg text-center py-3 w-full m-4"
+        >
+          See Details
+        </Link>
       </div>
     </div>
   );
